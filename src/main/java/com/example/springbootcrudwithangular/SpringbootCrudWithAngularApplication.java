482390d6bc7d4c7ce3208bb0d69b2ec42db4daca
minhdat2708 <minhdat01.dev@gmail.com>
2022-09-22 14:28:36 +0700

    first commit

---
+++ src/main/java/com/example/springbootcrudwithangular/SpringbootCrudWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudWithAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootcrudwithangular/SpringbootCrudWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudwithangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
